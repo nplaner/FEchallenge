58950bc8ade7959fd75afa050d6549b6be16da9e
Hang Xu <hangxu199371@gmail.com>
2021-03-03 15:45:54 -0800

    fetched city data

--- app/index.js
+++ app/index.js
@@ -1,24 +1,49 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { API_KEY } from '../apiKey.js';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+export default function App() {
+  const [selected, setSelected] = useState()
+  const [cityIds, setCityIds] = useState([2172797, 1705545, 1796228, 1006984, 1796247])
 
-class App extends React. Component{
+  const initFetch = () => {
+    
+    Promise.all(cityIds.map((id) => fetch(
+      `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${API_KEY}`
+    )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })))
+  }
 
-    render(){
+  useEffect(() => {
+    initFetch();
+  }, []);
 
-        return(
-
-            <div>Hello World</div>
-
-        )
-
-    }
-
+  return (
+    <div>Hello Woeqweqweqwerld</div>
+  )
 }
 
 
 
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+// import React from 'react';
+
+// export default function App() {
+//   const alertName = () => {
+//     alert('John Doe');
+//   };
+
+//   return (
+//     <div>
+//       <h3>This is a Functional Component</h3>
+//       <button onClick={alertName}>
+//         Alert
+//       </button>
+//     </div>
+//   );
+// };
