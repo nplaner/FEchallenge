a3e02035af4c9efaa4ae896052579e3f07ca5dad
Hang Xu <hangxu199371@gmail.com>
2021-03-04 11:13:09 -0800

    almost finished

--- components/selectionPanel.js
+++ components/selectionPanel.js
@@ -1,34 +1,31 @@
 import React, { useState } from 'react';
 import WeatherTable from './WeatherTable.js';
+import { Multiselect } from 'multiselect-react-dropdown';
 
 export default function SelectionPanel({ cityNames, cities }) {
   const [selected, setSelected] = useState([])
   const handleClick = (e) => {
-    if(e.target.checked) {
-      setSelected(arr => [...arr, e.target.value])
-      console.log(e.target.value)
-    }
-    else {
-      setSelected(selected.filter(city => city !== e.target.value))
-    }
+    (e.length ? setSelected(arr => [...arr, e]) : null)
+    // console.log('e', e)
   }
+  console.log('selected', selected) 
   const ids = []
   for(let i = 0; i < cities.length; i++) {
     for(let j = 0; j < selected.length; j++) {
-      if(cities[i].name === selected[j]) {
+      if(cities[i].name === selected[0][j]) {
         ids.push(cities[i].id)
       }
     }
   }
-  
-  const boxes = []
+
+  const options = []
   cityNames.map((el,i) => {
-    boxes.push(<label key={i}><input type="checkbox" onClick={handleClick} value={el}/>{el}</label>)
+    options.push({value: `${el}`, id: `${i}`})
   })
 
   return (
     <div>
-      {boxes}
+      <Multiselect options={cityNames} isObject={false} onSelect={handleClick}/>
       <WeatherTable ids={ids}/>
     </div>
   )
