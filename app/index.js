bd5db9913680d01ef0f27b84746c98fc80d64c38
Hang Xu <hangxu199371@gmail.com>
2021-03-03 22:29:14 -0800

    going back

--- app/index.js
+++ app/index.js
@@ -1,28 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { API_KEY } from '../apiKey.js';
 import ReactDOM from 'react-dom';
+import City from '../components/City'
+// import Select from 'react-select'
+import MultiSelect from 'react-multi-select-component';
 
 export default function App() {
-  const [selected, setSelected] = useState()
-  const [cityIds, setCityIds] = useState([2172797, 1705545, 1796228, 1006984, 1796247])
+  const Cities = [
+    { label: 'Cairns', value: '2172797' },
+    { label: 'Los Angeles', value: '1705545' },
+    { label: 'Linjiang', value: '1796228' },
+    { label: 'East London', value: '1006984' },
+    { label: 'Shanggu', value: '1796247' }
+  ]
+  // const [selected, setSelected] = useState([])
+  const [cityIds, setCityIds] = useState(['2172797','1705545', '1796228'])
 
-  const initFetch = () => {
+  // const initFetch = () => {
     
-    Promise.all(cityIds.map((id) => fetch(
-      `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${API_KEY}`
-    )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })))
+  //   Promise.all(cityIds.map((id) => fetch(
+  //     `http://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${API_KEY}`
+  //   )
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data)
+  //   })))
+  // }
+
+  // useEffect(() => {
+  //   initFetch();
+  // }, []);
+  const handleChange = () => {
+      setCityIds(arr => [...arr, label])
+      console.log(cityIds)
   }
 
-  useEffect(() => {
-    initFetch();
-  }, []);
-
   return (
-    <div>Hello Woeqweqweqwerld</div>
+    <div className="App">
+      <MultiSelect
+        options={Cities}
+        value={cityIds}
+        onChange={setCityIds}
+        // onChange={(opt)=> opt.forEach((x)=> {
+        //   setCityIds(cityIds =>[...cityIds, x.value])
+        //   console.log([...new Set(cityIds)])
+        // })}
+        />
+      <City ids = {cityIds}/>
+    </div>
   )
 }
 
@@ -30,20 +55,3 @@ export default function App() {
 
 
 ReactDOM.render(<App />, document.getElementById('app'))
-
-// import React from 'react';
-
-// export default function App() {
-//   const alertName = () => {
-//     alert('John Doe');
-//   };
-
-//   return (
-//     <div>
-//       <h3>This is a Functional Component</h3>
-//       <button onClick={alertName}>
-//         Alert
-//       </button>
-//     </div>
-//   );
-// };
